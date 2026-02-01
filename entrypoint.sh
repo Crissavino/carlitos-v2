01572859f41ddd3ac1785e095ac4c1bc8c005a45
cristiansavino <savinocristian89@gmail.com>
2026-02-01 03:46:40 +0200

    Use OPENCLAW_GATEWAY_TOKEN env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -91,4 +91,4 @@ echo "Dashboard API started (PID: $DASHBOARD_PID)"
 
 # Start gateway in foreground
 cd /app
-exec openclaw --dev gateway --bind lan --port 18789 --allow-unconfigured --verbose
+exec openclaw --dev gateway --bind lan --port 18789 --allow-unconfigured --token "${OPENCLAW_GATEWAY_TOKEN:-$TOKEN}"
